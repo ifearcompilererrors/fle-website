15c5a0c21ee54be14736a1ac4f5351560d5c6620
Jamie Alexandre <jamalex@gmail.com>
2013-04-07 16:59:41 -0700

    Updated static file settings.

--- fle_site/settings.py
+++ fle_site/settings.py
@@ -67,7 +67,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = PROJECT_PATH + '/static/'
+STATIC_ROOT = PROJECT_PATH + '/../static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fle_site',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
