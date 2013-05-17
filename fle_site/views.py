86c8420874eb8ba2caa71ca213ef6e83e4bb3b2c
Matthew O'Rourke <morourke2289@gmail.com>
2013-05-17 15:39:24 -0700

    Added info windows to map which display when markers are clicked
    Now, when a marker on the map is clicked, an info window pops up
    to inform the user where that marker is. City, region, and country name
    will be listed, unless we are missing any of these fields in which case
    the fields that we do have will be displayed (only region and country if no city
     or only country region and city are both missing).

--- fle_site/views.py
+++ fle_site/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render_to_response
 import settings
 from settings import GEOIPDAT
 import pygeoip
+import json
+import re
 
 gic = pygeoip.GeoIP(GEOIPDAT)
 
@@ -17,8 +19,21 @@ def involved(request):
 def map(request):    
     ips = open(settings.PROJECT_PATH + "/ips.txt").readlines()
     records = [gic.record_by_addr(item.strip()) for item in ips if item]
-    lat_lng = [(record['latitude'], record['longitude']) for record in records if record]
+    locations = []
+    existing_locations = set([(0, 0)])
+    for record in records:
+    	if record:
+	    	if (record['latitude'], record['longitude']) not in existing_locations:
+	    		name = [record['city'], record['region_name'], record['country_name']]
+	    		name = filter(lambda x: not re.match("^\d*$", x), name)
+	    		name = ", ".join(name)
+	    		locations.append({
+	    			"latitude": record['latitude'],
+	    			"longitude": record['longitude'],
+		    		"name": name,
+	    		})
+	    		existing_locations.add((record['latitude'], record['longitude']))
     # remove duplicates and remove null coordinates
-    lat_lng = list(set(lat_lng) - set([(0, 0)]))
-    return render_to_response('map.html', {"latlng": lat_lng})
+    # location_info = list(set(location_info) - set([(0, 0)]))
+    return render_to_response('map.html', {"locations": json.dumps(locations)})
 
